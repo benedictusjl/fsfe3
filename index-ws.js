f30244a67e25b7fad80b936775cfde7bf5b42eb0
Benedictus Jullian <henrybenedict98@gmail.com>
2023-03-17 12:31:34 +0700

    adding sqlite3 database to our server

--- index-ws.js
+++ index-ws.js
@@ -4,7 +4,6 @@ const app = express();
 
 app.get('/', function(req,res) {
     res.sendFile('index.html', {root: __dirname});
-
 });
 
 server.on('request', app);
@@ -18,6 +17,17 @@ const WebSocketServer = require('ws').Server;
 
 const wss = new WebSocketServer({server: server});
 
+process.on('SIGINT', () => {
+    console.log('sigint');
+    wss.clients.forEach((client) => {
+        client.close();
+    });
+
+    server.close(() => {
+       shutdownDB();
+    });
+});
+
 wss.on('connection', function connection(ws) {
     const numClients = wss.clients.size;
     console.log('Clients connected: ', numClients);
@@ -27,6 +37,10 @@ wss.on('connection', function connection(ws) {
    if( ws.readyState === ws.OPEN) {
         ws.send('Welcome to my server');
    };
+
+   db.run(`INSERT INTO visitors (count,time)
+        VALUES (${numClients}, datetime('now'))
+   `);
 
    ws.on('close', function close() {
         wss.broadcast(`Current visitors: ${numClients}`);
@@ -39,3 +53,29 @@ wss.broadcast = function broadcast(data) {
         client.send(data);
     });
 };
+// End websocket
+
+// begin databases
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err,row)=> {
+        console.log(row);
+    });
+}
+
+function shutdownDB() {
+    getCounts();
+    console.log('Shutting down DB');
+    db.close();
+}
